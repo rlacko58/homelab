6c6c4f482faa84b7a1738b6156e028cc41f10f2a
Laszlo Rafael <rlacko99@gmail.com>
2026-02-20 21:51:11 +0100

    Add IP Configuration for data/storage VLAN

--- hosts/eris/configuration.nix
+++ hosts/eris/configuration.nix
@@ -12,23 +12,46 @@
   networking.hostName = "eris";
   system.stateVersion = "25.11";
 
-  systemd.network.links."10-net0" = {
-    matchConfig.MACAddress = "38:05:25:36:41:58";
-    linkConfig.Name = "net0";
-  };
   networking = {
     useDHCP = false;
     enableIPv6 = false;
-    interfaces.net0.ipv4.addresses = [
-      {
-        address = "192.168.1.60";
-        prefixLength = 24;
-      }
-    ];
+    vlans.data0 = {
+      id = 100;
+      interface = "net0";
+    };
+
+    interfaces = {
+      net0 = {
+        ipv4.addresses = [
+          {
+            address = "192.168.1.60";
+            prefixLength = 24;
+          }
+        ];
+        # MTU 9000 to match data0 until migrated to other node
+        mtu = 9000;
+      };
+
+      data0 = {
+        ipv4.addresses = [
+          {
+            address = "172.10.10.60";
+            prefixLength = 23;
+          }
+        ];
+        mtu = 9000;
+      };
+    };
+
     defaultGateway = "192.168.1.1";
     nameservers = [
       "1.1.1.3"
       "1.0.0.3"
     ];
+  };
+
+  systemd.network.links."10-net0" = {
+    matchConfig.MACAddress = "38:05:25:36:41:58";
+    linkConfig.Name = "net0";
   };
 }
